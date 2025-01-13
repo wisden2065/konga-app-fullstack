2be593148a2c0eec51b870a18e689d97ad098bc3
Nnanyereugo <nnajiwisdom1998@gmail.com>
2025-01-13 13:46:24 +0100

    Implemented dynamic product filter based on active checkbox

--- frontend/src/components/Product/Product.jsx
+++ frontend/src/components/Product/Product.jsx
@@ -6,37 +6,39 @@ import { useContext, useState } from 'react'
 import ProductContextProvider, { ProductContext } from '../../context/ProductContext'
 
 
-const Product = ({name, price, category, image}) => {
+const Product = ({name, price, category, image, brand}) => {
     const [cartItemCount, setCartItemCount] = useState(0);
     const { activeProdCat, setActiveProdCat } = useContext(ProductContext)
 
         return <>
-                    <div className="productItem" category={category}>
+                    <div className="productItem" category={category} brand={brand} >
                                 <div className="saveProduct"><FontAwesomeIcon icon={faHeart} style={{color: '#ffff'}} /></div>
                                 <span className="topSpan">-9%</span>
                                 <div className="prodImg">
                                     <img src={image} />
                                 </div>
+                              <div className='prodDesc'>
                                 <p>{name}</p>
-                                <div className="prodPrice">
-                                    <h3><FontAwesomeIcon icon={faNairaSign} />{price}</h3>
-                                    <p>N <s>46,263</s></p>
-                                    <span className="prodSpan">-29%</span>
-                                </div>
-                                <p className="p">You save N11,300</p>
-                                <hr/>
-                                <p className="soldBy">Sold by <span>Official Apple Store</span></p>
-                                <div className="ratingCont">
-                                    <div>
-                                    <FontAwesomeIcon icon={faStar} style={{color: '#dedede'}} />
-                                    <FontAwesomeIcon icon={faStar} style={{color: '#dedede'}} />
-                                    <FontAwesomeIcon icon={faStar} style={{color: '#dedede'}} />
-                                    <FontAwesomeIcon icon={faStar} style={{color: '#dedede'}} />
-                                    <FontAwesomeIcon icon={faStar} style={{color: '#dedede'}} />
+                                    <div className="prodPrice">
+                                        <h3><FontAwesomeIcon icon={faNairaSign} />{price}</h3>
+                                        <p>N <s>46,263</s></p>
+                                        <span className="prodSpan">-29%</span>
                                     </div>
-                                    <p>No reviews yet</p>
-                                </div>
-                                <div className="prodBtn"><a><button>Add To Cart</button></a></div>
+                                    <p className="p">You save N11,300</p>
+                                    <hr/>
+                                    <p className="soldBy">Sold by <span>Official Apple Store</span></p>
+                                    <div className="ratingCont">
+                                        <div>
+                                        <FontAwesomeIcon icon={faStar} style={{color: '#dedede'}} />
+                                        <FontAwesomeIcon icon={faStar} style={{color: '#dedede'}} />
+                                        <FontAwesomeIcon icon={faStar} style={{color: '#dedede'}} />
+                                        <FontAwesomeIcon icon={faStar} style={{color: '#dedede'}} />
+                                        <FontAwesomeIcon icon={faStar} style={{color: '#dedede'}} />
+                                        </div>
+                                        <p>No reviews yet</p>
+                                    </div>
+                                    <div className="prodBtn"><a><button>Add To Cart</button></a></div>
+                              </div>
                     </div>  
                     {/* } */}
                </>
---
+++ frontend/src/components/ProductsCont/ProductsCont.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductsCont = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProductsCont
