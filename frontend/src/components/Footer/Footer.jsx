a5e490d16e936797e8c3b0f06219a64d6b8b2702
Nnanyereugo <nnajiwisdom1998@gmail.com>
2025-02-07 09:46:13 +0100

    Implemeted active state for bottom nav @media max-480px

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -8,33 +8,40 @@ import {faApple, faFacebook, faInstagram, faTwitter, faWhatsapp, faYoutube} from
 
 const Footer = () => {
 
-    const [ activeFooterTab, setActiveFooterTab ] = useState('active')
+    const [ activeFooterTab, setActiveFooterTab ] = useState('home')
 
   return (
     <>
            <div className='footer-nav'>
-             <a className={`icon-cont ${activeFooterTab}`}
-                // onClick={()=>{setActiveFooterTab()}}
+             <a className={`icon-cont ${activeFooterTab == 'home'? 'active': ''}`}
+                onClick={()=>{setActiveFooterTab('home')}}
              >
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
                     <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z"/>
                 </svg>
                 <span>Home</span>
              </a>
-             <a className='icon-cont'>
+             <a className={`icon-cont ${activeFooterTab == 'deals'? 'active': ''}`}
+                onClick={()=>{setActiveFooterTab('deals')}}
+             >
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tags" viewBox="0 0 16 16">
                     <path d="M3 2v4.586l7 7L14.586 9l-7-7zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586z"/>
                     <path d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3M1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1z"/>
                 </svg>
                 <span>Deals</span>
              </a>
-             <a className='icon-cont'>
+             <a 
+                className={`icon-cont ${activeFooterTab == 'cart'? 'active': ''}`}
+                onClick={()=>{setActiveFooterTab('cart')}}
+             >
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
                     <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/>
                 </svg>
                 <span>Cart</span>
              </a>
-             <a className='icon-cont'>
+             <a className={`icon-cont ${activeFooterTab == 'more'? 'active': ''}`}
+                onClick={()=>{setActiveFooterTab('more')}}
+             >
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter-right" viewBox="0 0 16 16">
                     <path d="M14 10.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5m0-3a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0 0 1h7a.5.5 0 0 0 .5-.5m0-3a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0 0 1h11a.5.5 0 0 0 .5-.5"/>
                 </svg>
