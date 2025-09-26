db359b39391536da803c432816bbc8c058446fa9
Nnanyereugo <nnajiwisdom1998@gmail.com>
2025-09-26 17:14:20 +0100

    Added more fs at Hero carousel slider

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import './Home.css'
 import slide1 from '../../assets/images/slider1.webp'
+import slide2 from '../../assets/images/slide2.gif'
+import slide3 from '../../assets/images/slide3.gif'
+import slide4 from '../../assets/images/slide5.webp'
+import slide5 from '../../assets/images/slide6.gif'
+import slide6  from '../../assets/images/slide7.gif'
+import slide7  from '../../assets/images/slide7.webp'
 import {images} from '../../assets/images/index'
 import { spanImages } from '../../assets/images/index'
 import ProdDealCard from '../../components/ProdDealCard/ProdDealCard'
@@ -49,10 +55,22 @@ const Home = () => {
                                 <img className='slider-img' src={slide1} alt="" />
                             </SwiperSlide>
                             <SwiperSlide>
-                                <img src={slide1} alt="" />
+                                <img className='slider-img' src={slide2} alt="" />
                             </SwiperSlide>
                             <SwiperSlide>
-                                <img src={slide1} alt="" />
+                                <img className='slider-img' src={slide3} alt="" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img className='slider-img' src={slide4} alt="" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img className='slider-img' src={slide4} alt="" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img className='slider-img' src={slide5} alt="" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img className='slider-img' src={slide6} alt="" />
                             </SwiperSlide>
                         </Swiper>
                         <div className='custom-next'>{'>'}</div>
